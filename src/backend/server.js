54384e32b874f76d5d423389a3a83ebb2d287eae
BleckWolf25 <119084160+JotaRYT@users.noreply.github.com>
2024-09-22 19:30:20 +0100

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -44,7 +44,6 @@ mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-
 // ------ NEWSLETTER ROUTES ------ \\
 
 // Create a transporter for Nodemailer using Gmail's SMTP server
