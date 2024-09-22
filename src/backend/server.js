2a5c845ef600a426a97ed553583d44237ba5db1a
BleckWolf25 <119084160+JotaRYT@users.noreply.github.com>
2024-09-22 22:10:46 +0100

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 // Get the directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
